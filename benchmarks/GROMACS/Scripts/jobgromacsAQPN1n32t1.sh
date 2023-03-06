e165c6a24da8777c281f4b0f593a47da7784a24c
Johan Hellsvik <hellsvik@kth.se>
2023-03-06 23:29:42 +0100

    Added template scripts for the AQP example

---
+++ benchmarks/GROMACS/Scripts/jobgromacsAQPN1n32t1.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+#SBATCH -J my_gmx_job
+#SBATCH -A pdc.staff
+#SBATCH -p s11-tst
+#SBATCH -t 1-00:00:00
+
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=32
+
+ml PDCTEST/22.06
+ml gromacs/2023-cpeGNU-22.06
+
+export OMP_NUM_THREADS=1
+
+echo "Script initiated at `date` on `hostname`"
+
+srun -n 32 gmx_mpi mdrun -multidir repl_* -nsteps 1000000 -resethway
+
+echo "Script finished at `date` on `hostname`"
